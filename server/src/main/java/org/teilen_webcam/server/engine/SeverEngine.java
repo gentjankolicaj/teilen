138442f87c4d609d693fc7eaef1200c15f232d9b
Gentjan Kolicaj <gentjankolicaj@yahoo.com>
2021-10-22 12:18:43 +0200

    New classes for engine.

---
+++ server/src/main/java/org/teilen_webcam/server/engine/SeverEngine.java
@@ -0,0 +1,7 @@
+package org.teilen_webcam.server.engine;
+
+public class SeverEngine {
+    private ChatEngine chatEngine;
+    private VideoEngine videoEngine;
+
+}
---
+++ server/src/main/java/org/teilen_webcam/server/engine/VideoEngine.java
@@ -0,0 +1,4 @@
+package org.teilen_webcam.server.engine;
+
+public class VideoEngine {
+}
